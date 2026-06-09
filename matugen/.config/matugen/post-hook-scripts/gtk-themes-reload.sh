740aece28e5fd8d2e4e1fa04542a9766e5efb5f8
Florian Ohldag <mrminecrafttv19@gmail.com>
2026-06-09 20:27:51 +0200

    remove comments

--- matugen/.config/matugen/post-hook-scripts/gtk-themes-reload.sh
+++ matugen/.config/matugen/post-hook-scripts/gtk-themes-reload.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# 1. Den normalen Farbwechsel-Toggle durchführen (für alle anderen GTK-Apps)
 current=$(gsettings get org.gnome.desktop.interface color-scheme)
 
 if [[ "$current" == "'prefer-dark'" ]]; then
@@ -11,13 +10,10 @@ else
     gsettings set org.gnome.desktop.interface color-scheme prefer-light
 fi
 
-# 2. DER TRICK FÜR NAUTILUS:
-# Wir senden ein Signal über D-Bus, das Nautilus zwingt, seine UI-Ressourcen neu zu laden,
-# OHNE dass der Prozess stirbt oder deine offenen Tabs geschlossen werden.
 if pgrep -x "nautilus" > /dev/null; then
     dbus-send --session --dest=org.gnome.Nautilus --type=method_call /org/gnome/Nautilus org.freedesktop.Actions.Activate string:'reload' array:objpath:[] dict:string:variant:{} &>/dev/null
     
-    # Falls der D-Bus-Reload in deiner Nautilus-Version zickt, nutzen wir den sanften "Weckruf" via gsettings-Erzwingung:
+
     gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
     sleep 0.05
     gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
