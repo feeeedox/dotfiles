7470416df8e94819bdd065471a6ebccfaed9b2d0
Florian Ohldag <mrminecrafttv19@gmail.com>
2026-06-26 11:13:38 +0200

    fix: correctly start the desktop portal after starting

--- hypr/.config/hypr/modules/autostart.lua
+++ hypr/.config/hypr/modules/autostart.lua
@@ -9,4 +9,6 @@ hl.on("hyprland.start", function ()
     hl.exec_cmd("wl-paste --type image --watch cliphist store &")
     hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=Hyprland")
     hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+    hl.exec_cmd("~/.config/hypr/scripts/xdg-portal-init.sh &")
 end)
+
